14f1a43ebc475d0d0937497b19dacb0bffb5d3e7
Sezerims <bullalioglu@gmail.com>
2023-10-06 18:28:23 +0300

    Change outline to box-shadow.

--- src/components/Custom.jsx
+++ src/components/Custom.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Custom = () => {
     return (
-        <div className="custom-vote vanished">
+        <div className="custom-vote">
             <p>🌟 Custom Vote 🌟</p>
             <input type="number"/>
             <div className="button-container">
